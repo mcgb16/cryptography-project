f6fec588657ab023f755e4b8505355d9025beb5e
Matheus Correa <matheus.barrosc@gmail.com>
2023-08-25 19:08:19 -0300

    Ajuste na tela para reutilizar componentes já criados, sem ficar criando duplicado.

--- main.py
+++ main.py
@@ -22,6 +22,7 @@ class MainPage(Page):
             encrypt_text_button.pack()
         elif self.title == 'Criptografar Textos':
             validate_input_len_cmd = self.root.register(self.validate_input_len)
+            self.count = 0
 
             self.name_label = tkinter.Label(self.root, text='Insira seu nome')
             self.name_label.pack()
@@ -52,6 +53,7 @@ class MainPage(Page):
         next_page.root.mainloop()
     
     def check_data(self):
+        self.count += 1
         data_name = self.insert_name.get()
         data_cpf = self.insert_cpf.get()
         try:
@@ -59,57 +61,62 @@ class MainPage(Page):
         except:
             data_text_to_encrypt = ''
         
-        validation_label = tkinter.Label(self.root, text='')
-        validation_label.pack()
-        
-        len_validation = func.verify_len_input(data_cpf, data_name, data_text_to_encrypt)
+        if self.count == 1:
+            self.create_common_widgets()
 
-        if len_validation == 'valid':
-            cpf_validation = func.verify_cpf(data_cpf)
-            
-            if cpf_validation == 'cpf valid':
-                encrypted_text = func.generate_crypt_data(data_text_to_encrypt)
+            len_validation = func.verify_len_input(data_cpf, data_name, data_text_to_encrypt)
 
-                encrypt_text_label = tkinter.Label(self.root, text='Texto criptografado:')
-                encrypt_text_label.pack()
+            if len_validation == 'valid':
+                cpf_validation = func.verify_cpf(data_cpf)
                 
-                encrypt_frame = tkinter.Frame(self.root)
-                encrypt_frame.pack(fill="both", expand=True)
+                if cpf_validation == 'cpf valid':
+                    encrypted_text = func.generate_crypt_data(data_text_to_encrypt)
+                    
+                    self.encrypt_text_show.insert("1.0", encrypted_text)
+                    self.encrypt_text_show.config(state='disabled')
+                    
+                    self.encrypt_text_scroll.config(command=self.encrypt_text_show.yview)
 
-                encrypt_text_scroll = tkinter.Scrollbar(encrypt_frame)
-                encrypt_text_scroll.pack(side="right", fill="y")
+                    unique_key = func.generate_unique_key()
 
-                encrypt_text_show = tkinter.Text(encrypt_frame, wrap="word", height=5, yscrollcommand=encrypt_text_scroll.set)
-                encrypt_text_show.insert("1.0", encrypted_text)
-                encrypt_text_show.config(state='disabled')
-                encrypt_text_show.pack(fill="both", expand=True)
-                encrypt_text_scroll.config(command=encrypt_text_show.yview)
+                    self.unique_key_show.insert("1.0", unique_key)
+                    self.unique_key_show.config(state='disabled')
+                    self.unique_key_scroll.config(command=self.unique_key_show.yview)
 
-                unique_key = func.generate_unique_key()
+                    save_on_db = func.send_text_to_db(unique_key, data_name, data_text_to_encrypt, data_cpf)
+                    self.validation_label.config(text=save_on_db)
 
-                unique_key_label = tkinter.Label(self.root, text='Key para descriptografar:')
-                unique_key_label.pack()
-
-                unique_key_frame = tkinter.Frame(self.root)
-                unique_key_frame.pack(fill="both", expand=True)
-
-                unique_key_scroll = tkinter.Scrollbar(unique_key_frame)
-                unique_key_scroll.pack(side="right", fill="y")
-
-                unique_key_show = tkinter.Text(unique_key_frame, wrap="word", height=2, yscrollcommand=unique_key_scroll.set)
-                unique_key_show.insert("1.0", unique_key)
-                unique_key_show.config(state='disabled')
-                unique_key_show.pack(fill="both", expand=True)
-                unique_key_scroll.config(command=unique_key_show.yview)
-
-                save_on_db = func.send_text_to_db(unique_key, data_name, data_text_to_encrypt, data_cpf)
-                save_on_db_label = tkinter.Label(self.root, text=save_on_db)
-                save_on_db_label.pack()
-
+                else:
+                    self.validation_label.config(text=cpf_validation)
             else:
-                validation_label.config(text=cpf_validation)
+                self.validation_label.config(text=len_validation)
         else:
-            validation_label.config(text=len_validation)
+            len_validation = func.verify_len_input(data_cpf, data_name, data_text_to_encrypt)
+
+            if len_validation == 'valid':
+                cpf_validation = func.verify_cpf(data_cpf)
+                
+                if cpf_validation == 'cpf valid':
+                    encrypted_text = func.generate_crypt_data(data_text_to_encrypt)
+
+                    self.encrypt_text_show.config(state='normal')
+                    self.encrypt_text_show.delete("1.0", "end")
+                    self.encrypt_text_show.insert("1.0", encrypted_text)
+                    self.encrypt_text_show.config(state='disabled')
+
+                    unique_key = func.generate_unique_key()
+
+                    self.unique_key_show.config(state='normal')
+                    self.unique_key_show.delete("1.0", "end")
+                    self.unique_key_show.insert("1.0", unique_key)
+                    self.unique_key_show.config(state='disabled')
+
+                    save_on_db = func.send_text_to_db(unique_key, data_name, data_text_to_encrypt, data_cpf)
+                    self.validation_label.config(text=save_on_db)
+                else:
+                    self.validation_label.config(text=cpf_validation)
+            else:
+                self.validation_label.config(text=len_validation)
         
     def validate_input_len(self, P, input_type):
         if input_type == 'name':
@@ -124,6 +131,33 @@ class MainPage(Page):
     def update_label_when_digit(self, event):
         self.current_text = self.insert_text_to_encrypt.get("1.0", "end-1c")
         self.label_var_per_digit.set(255 - len(self.current_text))
+
+    def create_common_widgets(self):
+        self.validation_label = tkinter.Label(self.root, text='')
+        self.validation_label.pack()
+
+        self.encrypt_text_label = tkinter.Label(self.root, text='Texto criptografado:')
+        self.encrypt_text_label.pack()
+        self.encrypt_frame = tkinter.Frame(self.root)
+        self.encrypt_frame.pack(fill="both", expand=True)
+
+        self.encrypt_text_scroll = tkinter.Scrollbar(self.encrypt_frame)
+        self.encrypt_text_scroll.pack(side="right", fill="y")
+
+        self.encrypt_text_show = tkinter.Text(self.encrypt_frame, wrap="word", height=5, yscrollcommand=self.encrypt_text_scroll.set)
+        self.encrypt_text_show.pack(fill="both", expand=True)
+
+        self.unique_key_label = tkinter.Label(self.root, text='Key para descriptografar:')
+        self.unique_key_label.pack()
+
+        self.unique_key_frame = tkinter.Frame(self.root)
+        self.unique_key_frame.pack(fill="both", expand=True)
+
+        self.unique_key_scroll = tkinter.Scrollbar(self.unique_key_frame)
+        self.unique_key_scroll.pack(side="right", fill="y")
+
+        self.unique_key_show = tkinter.Text(self.unique_key_frame, wrap="word", height=2, yscrollcommand=self.unique_key_scroll.set)
+        self.unique_key_show.pack(fill="both", expand=True)
 
 if __name__ == "__main__":
     main_page = MainPage('Cryptography App')
